2694d3039a11a70524cebc270006ad0bd8395696
slobodyanuck16 <slobodyanuck@users.noreply.github.com>
2020-03-30 13:41:24 +0300

    cm

--- task1/index.js
+++ task1/index.js
@@ -1,1 +1,9 @@
 //test
+const user = {
+    firstName = 'Tom',
+    lastName = 'Ford',
+    getFuulName: function() {
+        console.log(`${this.firstName} ${this.lastName}`);
+    }
+};
+user.getFuulName();
