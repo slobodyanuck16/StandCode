18e4cbaa3a725b07ad0a492be90798c2f43ca729
slobodyanuck16 <slobodyanuck@users.noreply.github.com>
2020-04-02 15:59:34 +0300

    с

---
+++ task2/index.js
@@ -0,0 +1,32 @@
+const dayTransactions = [
+    { currency: 'USD', amount:60, operation: 'sell' },
+    { currency: 'EUR', amount:160, operation: 'buy' },
+    { currency: 'EUR', amount:90, operation: 'sell' },
+];
+
+function getTotalBalance(transactionsList) {
+    const transformedTransactions = transactionsList.map(function(transactionObj) {
+        const key = transactionObj.currency;
+        const value = transactionObj.amount;
+
+        const obj = {
+            [key]: transactionObj.operation === 'sell' ? -value : value
+        };
+        return obj;
+    });
+
+    const result = transformedTransactions.reduce(function(acc, transactionObj) {
+        const currency = Object.keys(transactionObj);
+        let amount = Object.values(transactionObj);
+        const newValue = acc[currency] ? acc[currency] + amount : amount;
+        console.log(newValue);
+        amount = newValue;
+        Object.assign(acc, transactionObj);
+        return acc;
+    },{});
+    return result;
+}
+
+const result = getTotalBalance(dayTransactions);
+
+console.log(result);
